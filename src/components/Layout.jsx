187c941573bdc4185939f64d6506a3758a170a27
MHafidafandi <afandimboizz@gmail.com>
2023-06-15 13:38:09 +0700

    blur layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,6 +2,7 @@ import { NavbarContext } from "@/Context/NavbarContext";
 import React, { useContext } from "react";
 
 const Layout = ({ children, className = "" }) => {
+  const { isInvisible } = useContext(NavbarContext);
   const { toggleNavbarInvisible } = useContext(NavbarContext);
 
   const handleVisible = () => {
@@ -9,9 +10,10 @@ const Layout = ({ children, className = "" }) => {
   };
   return (
     <div
-      className={`w-full h-full inline-block z-0 bg-white p-32 xl:p-24 lg:p-16 md:p-12 sm:p-8 ${className} dark:bg-black`}
+      className={`w-full h-full inline-block z-0 bg-white p-32 xl:p-24 lg:p-16 md:p-12 sm:p-8 ${className} dark:bg-black ${
+        isInvisible ? "blur-none" : "blur-2xl"
+      }`}
       onClick={handleVisible}
-      onScroll={handleVisible}
     >
       {children}
     </div>
